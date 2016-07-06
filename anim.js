e0a0e896ee94828cde12b786d5d9e8ca29a80e0e
Rémi LEBLOND <remi.leblond@laposte.net>
2016-07-06 14:30:14 +0200

    Mise en forme

--- anim.js
+++ anim.js
@@ -1,12 +1,42 @@
 
 
-var myAnimation = anime({
-  targets: 'div',
+var myAnimationRed = anime({
+  targets: 'div.red',
   scale: {
     value: 1.2,
     duration: 5000,
     begin: 0,
     easing: 'easeOutExpo'
+  },
+  translateX: {
+    value: '25rem',
+    duration: 1000,
+    begin: 0,
+    easing: 'easeInOutExpo'
+  },
+  rotate: {
+    value: 180,
+    duration: 1000,
+    begin: 0,
+    easing: 'easeInOutExpo'
+  },
+  scale: {
+    value: 0.8,
+    duration: 1000,
+    begin: 500,
+    easing: 'easeInOutExpo'
+  },
+  direction: 'alternate',
+  loop: true
+});
+
+var myAnimationBlue = anime({
+  targets: 'div.blue',
+  scale: {
+    value: 1.2,
+    duration: 5000,
+    begin: 250,
+    easing: 'easeInOutExpo'
   },
   translateX: {
     value: '25rem',
